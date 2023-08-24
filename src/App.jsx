7b7d81a65d2a8aaf1505c37f0283fc3f8f880bf3
dwigunardi <dwigunardi@gmail.com>
2023-08-25 06:49:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Works,
+  Tech,
+  StarsCanvas,
+} from "./components";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
